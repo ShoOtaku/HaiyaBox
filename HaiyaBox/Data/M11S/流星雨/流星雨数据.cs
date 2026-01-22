bd02d9619e6098ceb1ff0b25ba6d7843c7f636e1
嗨呀 <991876906@qq.com>
2026-01-22 23:11:33 +0800

    流星雨数据更新

--- HaiyaBox/Data/M11S/流星雨/流星雨数据.cs
+++ HaiyaBox/Data/M11S/流星雨/流星雨数据.cs
@@ -4,5 +4,7 @@ namespace HaiyaBox.Data.M11S.流星雨;
 
 public class 流星雨数据 :IMechanismState
 {
-    
+    public static 流星雨数据 Instance { get; } = new();
+    public List<uint> 连线黑洞 = new();
+    public List<uint> 连线目标 = new();
 }
